63925b8a905d825ce0340ffb16332951cb68cbfc
Justin Love <git@JustinLove.name>
2014-06-04 07:51:34 -0500

    game sends an empty selection message, hopefully when its legal to select to things

--- ui/mods/autoselect_commander/autoselect_commander.js
+++ ui/mods/autoselect_commander/autoselect_commander.js
@@ -2,32 +2,38 @@
   "use strict";
 
   var subscribeToSelection = function() {
-    var selectionSubscription = model.selection.subscribe(function(payload) {
+    var original_selection = handlers.selection
+    var triggered = false
+    handlers.selection = function(payload) {
+      original_selection(payload)
       if (!payload) return
 
       console.info('AC: examining initial selection')
       var si = payload.spec_ids
       var sik = Object.keys(si)
-      if (sik.length == 1) {
+      if (sik.length == 0) {
+        if (!triggered) api.select.commander()
+        triggered = true
+      } else if (sik.length == 1) {
+        handlers.selection = original_selection
         if (sik[0].match('/pa/units/commanders/')) {
           if (si[sik[0]].length == 1) {
             api.camera.track(true)
           } else {
             console.info('AC: more than one commander, NOP')
-            setTimeout(api.select.empty, 200)
+            api.select.empty()
           }
         }
+      } else {
+        handlers.selection = original_selection
       }
-
-      selectionSubscription.dispose()
-    })
+    }
   }
 
   model.showLanding.subscribe(function(show) {
     if (show == false) {
       console.info('AC: Landed')
       subscribeToSelection()
-      setTimeout(api.select.commander, 300)
     }
   })
 })()
