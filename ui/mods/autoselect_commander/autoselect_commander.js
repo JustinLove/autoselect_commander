964fc271c3680d5ef6f05b10fea68e1c829ff490
Justin Love <git@JustinLove.name>
2014-06-04 02:20:11 -0500

    autodetect multiple commanders

--- ui/mods/autoselect_commander/autoselect_commander.js
+++ ui/mods/autoselect_commander/autoselect_commander.js
@@ -1,14 +1,33 @@
 (function() {
-  model.autoselectCommanderOnSpawn = ko.observable(true).extend({local: 'autoselect_commander_on_spawn'})
+  "use strict";
+
+  var subscribeToSelection = function() {
+    var selectionSubscription = model.selection.subscribe(function(payload) {
+      if (!payload) return
+
+      console.info('AC: examining initial selection')
+      var si = payload.spec_ids
+      var sik = Object.keys(si)
+      if (sik.length == 1) {
+        if (sik[0].match('/pa/units/commanders/')) {
+          if (si[sik[0]].length == 1) {
+            api.camera.track(true)
+          } else {
+            console.info('AC: more than one commander, NOP')
+            setTimeout(api.select.empty, 200)
+          }
+        }
+      }
+
+      selectionSubscription.dispose()
+    })
+  }
 
   model.showLanding.subscribe(function(show) {
-    if (show == false && model.autoselectCommanderOnSpawn() == true) {
-      setTimeout(function() {
-        api.select.commander()
-        api.camera.track(true)
-      }, 200)
+    if (show == false) {
+      console.info('AC: Landed')
+      subscribeToSelection()
+      setTimeout(api.select.commander, 300)
     }
   })
-
-  $('.div_launch_msg .div_message_display_cont').prepend('<div>Autoselect Commander: <input type="checkbox" data-bind="checked: autoselectCommanderOnSpawn"/></div>')
 })()
